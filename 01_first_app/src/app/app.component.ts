7ad434a1016f832a04bd7343cf656528eac06090
Chimoha31 <mokochii1108@gmail.com>
2022-12-10 20:11:31 -0800

    header & button components

--- 01_first_app/src/app/app.component.ts
+++ 01_first_app/src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title: string = '01_first_app';
-}
+export class AppComponent {}
