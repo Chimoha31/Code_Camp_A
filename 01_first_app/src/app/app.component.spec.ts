6a20d79c1a0d64e4d7d665aa1af8e039e01c7c1d
Chimoha31 <mokochii1108@gmail.com>
2022-12-10 19:50:14 -0800

    Replaced Old with New

--- 01_first_app/src/app/app.component.spec.ts
+++ 01_first_app/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first_app'`, () => {
+  it(`should have as title '01_first_app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first_app');
+    expect(app.title).toEqual('01_first_app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('first_app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('01_first_app app is running!');
   });
 });
