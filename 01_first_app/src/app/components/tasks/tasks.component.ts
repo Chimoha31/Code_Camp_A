c48693afb9ef2e1e40af83cfe1db9ac085193822
Chimoha31 <mokochii1108@gmail.com>
2022-12-11 23:38:31 -0800

    Fixing problem

--- 01_first_app/src/app/components/tasks/tasks.component.ts
+++ 01_first_app/src/app/components/tasks/tasks.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
