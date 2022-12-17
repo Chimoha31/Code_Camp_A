3a31d5c674fd69ebfe4105278a5cb424b2caae2d
Chimoha31 <mokochii1108@gmail.com>
2022-12-16 16:08:47 -0800

    Created product module

--- 03reservation_app/src/app/product-list/product-list.component.ts
+++ 03reservation_app/src/app/product-list/product-list.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
+  products: any = [1, 2, 3, 4];
+  
   constructor() {}
   ngOnInit(): void {}
 }
