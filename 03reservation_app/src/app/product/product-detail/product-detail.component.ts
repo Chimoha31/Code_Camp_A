26fd7c8fed7d75df706fa882fa51e9bcf3ab5fbd
Chimoha31 <mokochii1108@gmail.com>
2022-12-17 19:43:34 -0800

    Pass index for psth

--- 03reservation_app/src/app/product/product-detail/product-detail.component.ts
+++ 03reservation_app/src/app/product/product-detail/product-detail.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd } from '@angular/router';
+import { products } from '../../products';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent implements OnInit{
-  constructor() {}
-  ngOnInit(): void {}
+export class ProductDetailComponent implements OnInit {
+  product: any;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')!];
+    });
+  }
 }
