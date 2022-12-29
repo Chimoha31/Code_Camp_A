d7c6d6e7020896c365e6f9833fc8ab17755bba7e
Chimoha31 <mokochii1108@gmail.com>
2022-12-29 08:40:10 -0800

    Done:clap:

--- 04todo01/src/app/app.component.ts
+++ 04todo01/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = '04todo01';
+  todos: Todo[] = [];
+  newTodo: string;
+
+  // Create Todo
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo = '';
+    } else {
+      alert('Please enter Todo');
+    }
+  }
+
+  // Done Todo
+  done(id: number) {
+    this.todos[id].isCompleted = !this.todos[id].isCompleted;
+  }
+
+  // Delete Todo
+  remove(id: number) {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
 }
