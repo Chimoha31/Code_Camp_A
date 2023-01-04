a749f635dcd7d60bc672b28ee320e1c8c4cf8582
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-04 15:49:18 -0800

    created some components

--- 02anguler/src/app/app.module.ts
+++ 02anguler/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './Container/container.component';
+import { NavComponent } from './nav/nav.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    NavComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
