c6b48cfcd1226b3320c6f5f4cf1493b23b8a71cc
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-04 14:55:56 -0800

    Clear 02angular

--- 02anguler/src/app/app.component.ts
+++ 02anguler/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = '02anguler';
+  message = 'I am a frontend developer';
 }
--- 02anguler/src/app/app.module.ts
+++ 02anguler/src/app/app.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TopbarComponent } from './components/topbar/topbar.component';
 import { RouterModule } from '@angular/router';
-import { ListsComponent } from './components/lists/lists.component';
 
 @NgModule({
-  declarations: [AppComponent, TopbarComponent, ListsComponent],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: 'lists', component: ListsComponent }
-    ]),
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
