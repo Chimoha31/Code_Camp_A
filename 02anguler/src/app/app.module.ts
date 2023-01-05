a5d7fe398391f34c73c82cbe03a8eeb35e975c7b
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-05 01:16:03 -0800

    Installed bootstrap

--- 02anguler/src/app/app.module.ts
+++ 02anguler/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { ContainerComponent } from './Container/container.component';
 import { NavComponent } from './nav/nav.component';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { NotificationComponent } from './notification/notification.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    NotificationComponent
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ 02anguler/src/app/notification/notification.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: `<div class="alert alert-primary">
+    <p>
+      This website uses cookies to provide better user experience (notification)
+    </p>
+  </div>`,
+  styles: [
+    '.notification-div{margin: 10px 0px; padding: 10px 20px; background-color: #fad7a0; text-align: center}',
+    'p{font-size: 14px; font-weight: lighter}'
+  ],
+})
+export class NotificationComponent {}
