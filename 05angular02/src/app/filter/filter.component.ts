d77e74c348f5fa0a8f1fa0b028a7e444b6dcacf7
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-06 01:22:56 -0800

    @input

--- 05angular02/src/app/filter/filter.component.ts
+++ 05angular02/src/app/filter/filter.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-filter',
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.css']
+  styleUrls: ['./filter.component.css'],
 })
 export class FilterComponent {
-
+  @Input() all: number = 0;
+  @Input() free: number = 0;
+  @Input() premium: number = 0;
 }
