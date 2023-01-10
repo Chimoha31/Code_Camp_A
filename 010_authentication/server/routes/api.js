703eee3eed1dc776b48da791b73b76d865bc62fe
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-10 15:54:05 -0800

    Done server

--- 010_authentication/server/routes/api.js
+++ 010_authentication/server/routes/api.js
@@ -1,11 +1,122 @@
 const router = require("express").Router();
+const User = require("../models/user");
 
 // router.get("/", (req, res) => {
 //   res.send("From Api route");
 // });
 
-router.post("/resister", (req, res) => {
+// Resister (Create User)
+router.post("/resister", async (req, res) => {
+  try {
+    const newUser = await new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const user = await newUser.save();
+    return res.status(200).json({ msg: "Succesfully added user", data: user });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).send("There is no user...");
+
+    const validPassword = req.body.password === user.password;
+    if (!validPassword) return res.status(500).json("Not match password..");
+    return res.status(200).json({ msg: "Matched Password", data: user });
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// hard code
+router.get('/events', (req,res) => {
+  let events = [
+    {
+      "_id": "1",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "2",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "3",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "4",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "5",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "6",
+      "name": "Auto Expo",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    }
+  ]
+  res.json(events)
 })
+
+router.get('/special', (req, res) => {
+  let specialEvents = [
+    {
+      "_id": "1",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "2",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "3",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "4",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "5",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    },
+    {
+      "_id": "6",
+      "name": "Auto Expo Special",
+      "description": "lorem ipsum",
+      "date": "2012-04-23T18:25:43.511Z"
+    }
+  ]
+  res.json(specialEvents)
+})
+
 
 module.exports = router;
