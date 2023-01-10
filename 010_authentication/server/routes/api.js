7fae5dc425cb8da6c103d0c51aedb437eed21b82
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-10 14:54:50 -0800

    Implementing resister

---
+++ 010_authentication/server/routes/api.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+
+// router.get("/", (req, res) => {
+//   res.send("From Api route");
+// });
+
+router.post("/resister", (req, res) => {
+
+})
+
+module.exports = router;
---
+++ 010_authentication/server/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const PORT = 8000;
+
+const api = require("./routes/api");
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello server.js");
+});
+
+// Connect MongoDB
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Successfully connected to mongodb 🎉");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//
+app.use("/api", api);
+
+app.listen(PORT, (req, res) => {
+  console.log("Server is running on " + PORT + " 🏃🏼‍♀️💨");
+});
