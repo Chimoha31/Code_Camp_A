f93588c46828636e77dde586b0906c62b2b79140
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-10 16:49:31 -0800

    Created pages

--- 010_authentication/client/src/app/app.module.ts
+++ 010_authentication/client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResisterComponent } from './resister/resister.component';
+import { LoginComponent } from './login/login.component';
+import { EventsComponent } from './events/events.component';
+import { SpecialComponent } from './special/special.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResisterComponent,
+    LoginComponent,
+    EventsComponent,
+    SpecialComponent
   ],
   imports: [
     BrowserModule,
