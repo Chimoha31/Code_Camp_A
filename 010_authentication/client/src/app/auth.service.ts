375f50f40dcb7457fce331b336c8463f03e1dcf7
Chiho Maekawa <mokochii1108@gmail.com>
2023-01-11 13:59:29 -0800

    Done :clap:

--- 010_authentication/client/src/app/auth.service.ts
+++ 010_authentication/client/src/app/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
   private _registerUrl = 'http://localhost:8000/api/register';
   private _loginUrl = 'http://localhost:8000/api/login';
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private _router: Router) {}
 
   registerUser(user: any) {
     return this.http.post<any>(this._registerUrl, user);
@@ -19,6 +20,11 @@ export class AuthService {
     return !!localStorage.getItem('token');
   }
   getToken() {
-    return localStorage.getItem('token')
+    return localStorage.getItem('token');
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/events']);
+    console.log('logged out')
   }
 }
